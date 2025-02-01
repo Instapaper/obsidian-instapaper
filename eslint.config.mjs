43814de8099fcfc7aad7ecd260653068eb4dd741
Jon Parise <jon@indelible.org>
2025-02-01 12:28:35 -0500

    Retire unused ESLint rules (#81)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,13 +9,6 @@ export default tseslint.config(
     {
         ignores: ["main.js"],
         rules: {
-            "no-prototype-builtins": "off",
-            "no-unused-vars": "off",
-
             "@typescript-eslint/ban-ts-comment": "off",
-            "@typescript-eslint/no-empty-function": "off",
-            "@typescript-eslint/no-unused-vars": ["error", {
-                args: "none",
-            }],
         },
     });
